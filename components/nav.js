142c5fa7e6fcf5f65f68a409aa6a4c1cc098865e
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-03-16 13:31:22 -0700

    cleanup

--- components/nav.js
+++ components/nav.js
@@ -5,7 +5,6 @@ const Nav = () => {
   const user = useUser()
   const username = user && user.username
 
-
   return (
     <div className="navbar mb-8 shadow-lg bg-neutral text-neutral-content">
       <div className="flex-1">
@@ -34,34 +33,6 @@ const Nav = () => {
             <a className="btn btn-default">Login</a>
           </Link>
         )}
-      </div>
-    </div>
-  )
-
-  return (
-    <div className="navbar mb-8 shadow-lg bg-neutral text-neutral-content">
-      <div className="flex-1">
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal p-0">
-          {user ? (
-            <li tabIndex="0" className="dropdown">
-              <a className="btn btn-outline">
-                {user.username}
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-              </a>
-              <ul className="p-2 bg-base-100">
-                <li><a href="/api/logout">Logout</a></li>
-              </ul>
-            </li>
-          ) : (
-            <li>
-              <Link href="/login">
-                <a>Login</a>
-              </Link>
-            </li>
-          )}
-        </ul>
       </div>
     </div>
   )
