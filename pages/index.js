5fb3fa3d57fcab56e977cfb5179f2819ffc6ea9f
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-03-16 15:42:21 -0700

    main: small style tweaks

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,55 @@
+import Link from 'next/link'
 import { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 import Layout from '../components/layout'
+
+const Fetching = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 rotate" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
+  </svg>
+)
 
 const Home = () => {
   const { user } = useContext(GlobalContext)
   const { fetching, username, authenticated } = user;
 
-  if (authenticated === null || authenticated === false) {
+  if (authenticated === null) {
     return (
-      <Layout></Layout>
+      <Layout>
+        <div className="card bg-base-100 shadow-xl">
+          <div className="card-body w-[360px] h-[120px]" />
+        </div>
+
+      </Layout>
+    )
+  }
+
+  if (authenticated === false) {
+    return (
+      <Layout>
+        <div className="card bg-base-100 shadow-xl">
+          <div className="card-body w-[360px] h-[120px]">
+            <Link href="/login">
+              <a className="btn btn-outline">Login</a>
+            </Link>
+          </div>
+        </div>
+      </Layout>
     )
   }
 
   return (
     <Layout>
       <div className="card bg-base-100 shadow-xl">
-        <div className="card-body w-[360px]">
+        <div className="card-body w-[360px] h-[120px]">
           {username && (
-            <h3 className="text-xl">
+            <h3 className="text-2xl pt-2">
               Hiya, <code>{user.username}</code>
             </h3>
           )}
           {fetching && (
             <div className="m-auto">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 rotate" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
-              </svg>
+              <Fetching />
             </div>
           )}
         </div>
