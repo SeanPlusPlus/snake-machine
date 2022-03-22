c630dce0708c70899dc17cae422256c507d503d6
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-03-22 14:53:39 -0700

    DRAFTS: drafts on home page

---
+++ components/drafts.js
@@ -0,0 +1,31 @@
+import { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+const Drafts = () => {
+  const { user } = useContext(GlobalContext)
+  const { username, fetching, authenticated } = user
+  const drafts = [
+    {
+      name: 'foo'
+    },
+    {
+      name: 'bar'
+    }
+  ]
+
+  return (
+    <div>
+      {drafts.map((d) => (
+        <div className="card bg-base-100 shadow-xl m-2">
+          <div className="card-body">
+              <div className="m-auto">
+                {d.name}
+              </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Drafts
