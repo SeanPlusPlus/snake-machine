57379bf2bc033da87de735a6fd54a25ce10459ea
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-03-22 15:56:41 -0700

    DRAFTS: get drafts

--- components/drafts.js
+++ components/drafts.js
@@ -1,24 +1,31 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 
 const Drafts = () => {
+  const [ drafts, setDrafts ] = useState([])
   const { user } = useContext(GlobalContext)
   const { username, fetching, authenticated } = user
-  const drafts = [
-    {
-      name: 'foo'
-    },
-    {
-      name: 'bar'
-    }
-  ]
+
+  async function getDrafts() {
+    const res = await fetch('/api/draft')
+    const json = await res.json()
+    return json
+  }
+
+  getDrafts().then((data) => {
+    setDrafts(data.drafts)
+  })
 
   return (
     drafts.map((d, i) => (
       <div key={i} className="card bg-base-100 shadow-xl m-2">
         <div className="card-body">
-            <div className="m-auto">
-              {d.name}
+            <div className="m-auto text-left">
+              <span className="text-xl">{d.name}</span>
+              <div className="divider" />
+              <ul>
+                {d.items.map((item, idx) => <li key={idx}>{item.title}</li>)}
+              </ul>
             </div>
         </div>
       </div>
