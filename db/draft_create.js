f643318f1cef41ce61dbcc17192a7a350f1a7fe2
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-03-22 12:02:49 -0700

    DRAFTS: draft create with ref

--- db/draft_create.js
+++ db/draft_create.js
@@ -11,13 +11,53 @@ const client = new faunadb.Client({
   scheme: 'https',
 })
 
-async function createDraft({ items, ref }) {
+async function findUser({ username }) {
+  const index = 'users_by_username'
+  const item = await client.query(
+    q.Select([0],
+      q.Paginate(
+        q.Match(
+          q.Index(index),
+          username
+        )
+      )
+    )
+  )
+  const ref = item[1].value.id
+  const collection = 'users'
+  const user = await client.query(
+    q.Get(
+      q.Ref(
+        q.Collection(collection),
+        ref
+      )
+    )
+  )
+
+  return user
+}
+
+async function createDraft({ items, username }) {
+  let user = null
+
+  // check for username
+  try {
+    user = await findUser({ username })
+  } catch {
+    user = false
+  }
+
+  if (user === false) {
+    return false
+  }
+
+  console.log(user);
  
   const collection = 'drafts'
   const new_draft = await client.query(
     q.Create(
       q.Collection(collection),
-      { data: { items } }
+      { data: { items, userRef: user.ref } }
     )
   )
 
@@ -26,13 +66,14 @@ async function createDraft({ items, ref }) {
   }
 }
 
-const draft = { 
+const draft = {
+  username: 'sean',
   items: [
     {
-      title: 'foo'
+      title: 'hello'
     },
     {
-      title: 'bar'
+      title: 'world'
     }
   ]
 }
