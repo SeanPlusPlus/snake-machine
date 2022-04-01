78928d5c7bdc8b10bc7eac36626da234dd75f3c6
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-01 14:50:57 -0700

    main: scaffold out league create

---
+++ pages/league/create.js
@@ -0,0 +1,11 @@
+import Layout from "../../components/layout"
+
+const LeagueCreate = () => {
+  return (
+    <Layout>
+      <h1 className="text-4xl text-left bottom-2 border-b-2 border-sky-500">New League</h1>
+    </Layout>
+  )
+}
+
+export default LeagueCreate
