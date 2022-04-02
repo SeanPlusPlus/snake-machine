a15df767bede16b5f7856f6a691862e253e1037c
Sean Stephenson <Sean.M.Stephenson@disney.com>
2022-04-02 15:52:23 -0700

    main: add to modal

--- pages/league/create.js
+++ pages/league/create.js
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import Layout from "../../components/layout"
+import Fetching from '../../components/fetching'
 
 const LeagueCreate = () => {
-  const [values, setValues] = useState({
+  const [ submitting, setSubmitting ] = useState(null)
+  const [ league, setLeague ] = useState({})
+  const [ modal, setModal ] = useState('')
+  const [ values, setValues ] = useState({
     league_name: '',
     members: '',
     items: '',
   })
+
+  const handleCancel = () => {
+    setModal('')
+  }
   
   const handleLeagueName = (e) => {
     e.persist()
@@ -31,15 +39,24 @@ const LeagueCreate = () => {
       items: e.target.value,
     }))
   }
+
+  const randomize = () => {
+    console.log(league);
+  }
   
   const handleSubmit = async (e) => {
     e.preventDefault()
     const { league_name, members, items } = values
-    const league = {
+    setLeague({
       name: league_name,
       members: members.split('\n'),
       items: items.split('\n'),
-    }
+    })
+    setModal('modal-open')
+  }
+
+  const handleConfirm = async () => {
+    setSubmitting(true)
     const options = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -89,6 +106,66 @@ const LeagueCreate = () => {
 
         </div>
       </div>
+
+      <div className={`modal ${modal}`}>
+        <div className="modal-box">
+          <h3 className="font-bold text-2xl flex border-sky-500 border-b-2">
+            {league.name}
+          </h3>
+  
+          <div className="text-left pt-4">
+            <span className="text-lg">
+              Draft Order
+            </span>
+            <ul className="pl-4">
+              {league.members && league.members.map((m) => (
+                <li key={m} className="list-decimal text-sm">
+                  {m}
+                </li>
+              ))}
+            </ul>
+            <div
+              className="mt-1 link text-sky-500 hover:cursor-pointer hover:text-sky-600 flex text-md"
+              onClick={randomize}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+              </svg>
+              <span className="pl-1">
+                Randomize
+              </span>
+            </div>
+          </div>
+
+          <div className="text-left pt-7">
+            <span className="text-lg">
+              Draft Items
+            </span>
+            <ul className="pl-4">
+              {league.items && league.items.map((i) => (
+                <li key={i} className="list-decimal text-sm">
+                  {i}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+
+          <div className="modal-action pt-5">
+            {submitting ? (
+                <div className="border-2 rounded-md pl-8 pr-8 pt-2 pb-1">
+                  <Fetching />
+                </div>
+            ) : (
+              <>
+                <label htmlFor="my-modal" className="btn" onClick={handleCancel}>Cancel</label>
+                <label htmlFor="my-modal" className="btn btn-secondary" onClick={handleConfirm}>Confirm</label>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+
     </Layout>
   )
 }
